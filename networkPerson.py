aeda6de7d719080b79dfae350d3e07f32d967c94
Jason Zhang <sigmarising@hotmail.com>
2019-05-04 20:32:39 +0800

    MOD: the complete person graph builder in networkPerson

--- networkPerson.py
+++ networkPerson.py
@@ -4,53 +4,10 @@ import settings
 import networkx as nx
 import matplotlib.pyplot as plt
 from collections import Counter
+from networkx.readwrite import json_graph
 from module.ColorLogDecorator import ColorLogDecorator
 
 INPUT_DIR = os.path.join(settings.INPUT_PATH, "nerResult")
-
-
-def __test_xy():
-    def __check(file_x, file_y):
-        file_x_path = os.path.join(INPUT_DIR, file_x)
-        file_y_path = os.path.join(INPUT_DIR, file_y)
-
-        with open(file_x_path, 'r+', encoding='utf-8', errors='ignore') as f_x:
-            raw_x = json.load(f_x)
-        with open(file_y_path, 'r+', encoding='utf-8', errors='ignore') as f_y:
-            raw_y = json.load(f_y)
-
-        counter_x = Counter(raw_x["location"])
-        counter_y = Counter(raw_y["location"])
-
-        sum_common = 0
-        sum_uncommon = 0
-
-        for k in counter_x.keys():
-            if k in counter_y.keys():
-                sum_common += counter_x[k] + counter_y[k]
-            else:
-                sum_uncommon += counter_x[k]
-
-        for k in counter_y.keys():
-            if k not in counter_x.keys():
-                sum_uncommon += counter_y[k]
-
-        common_str = raw_x["dynasty"] + "_" + raw_x["author"] + " and " + raw_y["dynasty"] + "_" + raw_y["author"]
-        if sum_common > sum_uncommon:
-            char = '>'
-        elif sum_common < sum_uncommon:
-            char = '<'
-        else:
-            char = '='
-        msg = "common {2} uncommon: {0} {2} {1} in {3}".format(sum_common, sum_uncommon, char, common_str)
-        if sum_uncommon != 0 and sum_common != 0 and char in ['>', '=']:
-            print(ColorLogDecorator.blue(msg))
-
-    files_list = os.listdir(INPUT_DIR)
-    for i in range(0, len(files_list)):
-        for j in range(i, len(files_list)):
-            if not i == j:
-                __check(files_list[i], files_list[j])
 
 
 def main():
@@ -71,18 +28,18 @@ def main():
 
         for k in counter_x.keys():
             if k in counter_y.keys():
-                sum_common += counter_x[k] + counter_y[k]
+                sum_common += 1
             else:
-                sum_uncommon += counter_x[k]
+                sum_uncommon += 1
 
         for k in counter_y.keys():
             if k not in counter_x.keys():
-                sum_uncommon += counter_y[k]
+                sum_uncommon += 1
 
         if sum_common >= sum_uncommon:
-            return True
+            return True, sum_common
         else:
-            return False
+            return False, 0
 
     ColorLogDecorator.active()
     print(ColorLogDecorator.green("- START -"))
@@ -93,12 +50,20 @@ def main():
         print(ColorLogDecorator.blue("Handling: {}".format(files_list[i])))
         for j in range(i, len(files_list)):
             if i != j:
-                if __check_sum(files_list[i], files_list[j]):
+                result, value = __check_sum(files_list[i], files_list[j])
+                if result:
                     author1 = files_list[i].split('.')[0]
                     author2 = files_list[j].split('.')[0]
-                    graph.add_edge(author1, author2)
-    nx.draw_random(graph)
-    plt.savefig("test.png")
+                    graph.add_edge(author1, author2, weight=value)
+
+    output_path = os.path.join(settings.OUTPUT_PATH, "person")
+    with open(os.path.join(output_path, "result.json"), 'w+', encoding='utf-8', errors='ignore') as f:
+        json.dump(json_graph.node_link_data(graph), f, ensure_ascii=False, indent=4)
+
+    nx.draw_networkx(graph, node_size=10, width=0.5)
+    plt.savefig(os.path.join(output_path, "testResult.png"))
+
+    print(ColorLogDecorator.green("- DONE -"))
 
 
 if __name__ == '__main__':
